a086fb654c2fd79f5041db1d3b9a1bcf1717b0b0
Gregor Holzmann <debrisplease@gmail.com>
2018-09-19 15:44:21 -0700

    update createJob schema

--- src/jobs/schemas/job.schema.ts
+++ src/jobs/schemas/job.schema.ts
@@ -2,6 +2,5 @@ import * as mongoose from 'mongoose'
 
 export const JobSchema = new mongoose.Schema({
   title: String,
-  salary: Number,
-  _id: String
+  salary: Number
 })
--- src/jobs/types/job.interface.ts
+++ src/jobs/types/job.interface.ts
@@ -1,7 +1,6 @@
 export interface Job {
   title: string
   salary: number
-  _id: number
 }
 
 export interface NewJob {
